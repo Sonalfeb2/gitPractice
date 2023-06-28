c9510c041e8ee30e70f2f15e9da6b9ab0f92395b
Sonal Agrawal <sonalfeb7@gmail.com>
2023-06-28 10:52:02 +0530

    Add Query Selector

--- demo.js
+++ demo.js
@@ -1,11 +1,17 @@
-console.dir(document)
-var lists = document.getElementsByTagName
-('li');
+// console.dir(document);
+// var lists = document.getElementsByTagName("li");
 
-for(var i=0;i<lists.length;i++){
-    lists[i].style.fontWeight ='bold';
-    if(i==2){
+// for (var i = 0; i < lists.length; i++) {
+//   lists[i].style.fontWeight = "bold";
+//   if (i == 2) {
+//     lists[2].style.color = "green";
+//   }
+// }
 
-        lists[2].style.color = 'green';
-    }
-}
+var listsQuery = document.querySelectorAll("li");
+//nth child is not supporting at my chrome. It was defined as null.
+listsQuery[2].style.color = "green";
+
+listsQuery[3].style.display = "none";
+
+
