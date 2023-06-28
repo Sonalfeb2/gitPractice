1819b4482091fbeed77b362e3b2a86252f5752ba
Sonal Agrawal <sonalfeb7@gmail.com>
2023-06-28 10:20:42 +0530

    Added JS file for Styling

--- demo.js
+++ demo.js
@@ -1,1 +1,11 @@
-console.log("I love git")
+console.dir(document)
+var lists = document.getElementsByClassName
+('list-group-item');
+
+for(var i=0;i<lists.length;i++){
+    lists[i].style.fontWeight ='bold';
+    if(i==2){
+
+        lists[2].style.color = 'green';
+    }
+}
