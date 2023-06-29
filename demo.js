c1855a3d56015101cc4de5f4c2bc9732ce3a211a
Sonal Agrawal <sonalfeb7@gmail.com>
2023-06-29 20:34:00 +0530

    Added next Sliblings Nodes

--- demo.js
+++ demo.js
@@ -22,4 +22,5 @@ console.log(items.lastElementChild);
 console.log(items.lastChild);
 console.log(items.firstElementChild);
 console.log(items.firstChild);
-
+console.log(items.nextSibling);
+console.log(items.nextElementSibling);
