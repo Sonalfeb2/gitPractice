8e8888af5fa4c58cc017a3a3d3415bb4bcd6bf11
Sonal Agrawal <sonalfeb7@gmail.com>
2023-06-29 21:37:59 +0530

    Add Item in List

---
+++ main.js
@@ -0,0 +1,19 @@
+var form = document.getElementById("add-form");
+var itemList = document.getElementById('items');
+form.addEventListener('submit', AddItem);
+function AddItem(e){
+    e.preventDefault();
+    var itemValue = document.getElementById('item').value;
+    var newList = document.createElement('li');
+    newList.className = 'list-group-item';
+    var text = document.createTextNode(itemValue);
+    newList.appendChild(text);
+    console.log(newList)
+    var button = document.createElement('button');
+    button.className = "btn btn-danger btn-sm float-right delete";
+    
+    var btext = document.createTextNode('X');
+    button.appendChild(btext);
+    newList.appendChild(button);
+    itemList.appendChild(newList);
+}
